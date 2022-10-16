ccffab47556dc03d6d2e1ea6cb6485d5ed548fe6
maximus-sunshine <maxsun96@gmail.com>
2022-10-16 16:10:06 -0400

    10/14 updates

--- bagelizer.py
+++ bagelizer.py
@@ -10,51 +10,58 @@ import nomad_tools as nomad
 # set layout to fill screen
 st.set_page_config(layout="wide")
 
-# set paths for Streamlit hosted app
-logo = './images/logo_nomad.png'
+# create tabs
+tab1, tab2 = st.tabs(["Bagel Sales", "Bagel Labor"])
 
-# start printing stuff to the page, starting with a logo
-st.image(logo, width=100)
+with tab1:
 
-"""
-# Bagelizer 9000
-the latest & greatest in bagel software technology.
+    # set paths for Streamlit hosted app
+    logo = './images/logo_nomad.png'
 
-###### Upload a Modifier Sales report from Square.
-"""
+    # start printing stuff to the page, starting with a logo
+    st.image(logo, width=100)
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
+    """
+    # Bagelizer 9000
+    the latest & greatest in bagel software technology.
+    
+    ###### Upload a Modifier Sales report from Square.
+    """
 
-    # import & process modifier sales report - only care about "Bagel Flavor"
-    df_mod = nomad.import_modifier_sales(uploaded_file)
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
 
-    # create & display flavor and category summaries above full dataframe
-    flavors, categories = nomad.create_summary(df_mod)
+        # import & process modifier sales report - only care about "Bagel Flavor"
+        df_mod = nomad.import_modifier_sales(uploaded_file)
 
-    col1, col2, col3 = st.columns(3)
+        # create & display flavor and category summaries above full dataframe
+        flavors, categories = nomad.create_summary(df_mod)
 
-    with col1:
-        st.header("FLAVOR BREAKDOWN")
+        col1, col2, col3 = st.columns(3)
 
-        # display flavor breakdown and total
-        st.dataframe(flavors)
-        st.write(flavors['Quantity'].sum())
+        with col1:
+            st.header("FLAVOR BREAKDOWN")
 
-        # pie chart from flavors
-        fig = px.pie(flavors, values='Quantity', names='Flavor', title='Bagel Flavor Breakdown')
-        fig.update_traces(textinfo='label+value+percent')
-        st.plotly_chart(fig, use_container_width=True)
+            # display flavor breakdown and total
+            st.dataframe(flavors)
+            st.write(flavors['Quantity'].sum())
 
-    with col2:
-        st.header("CATEGORY BREAKDOWN")
+            # pie chart from flavors
+            fig = px.pie(flavors, values='Quantity', names='Flavor', title='Bagel Flavor Breakdown')
+            fig.update_traces(textinfo='label+value+percent')
+            st.plotly_chart(fig, use_container_width=True)
 
-        # display category breakdown & total
-        st.dataframe(categories)
-        st.write(categories['Bagels'].sum())
+        with col2:
+            st.header("CATEGORY BREAKDOWN")
 
-    with col3:
-        st.header('MODIFIER SALES DATA')
-        st.dataframe(df_mod)
+            # display category breakdown & total
+            st.dataframe(categories)
+            st.write(categories['Bagels'].sum())
 
+        with col3:
+            st.header('MODIFIER SALES DATA')
+            st.dataframe(df_mod)
 
+with tab2:
+
+    st.write('coming soon')
